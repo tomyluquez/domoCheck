7e9311d3a3bdc4aa45b8cf6671a9a7a6a3a904e2
Tomas luquez <luquez1431@gmail.com>
2023-07-11 10:01:31 -0300

    ordene actividades por dia

--- src/services/ordenarActividades.js
+++ src/services/ordenarActividades.js
@@ -3,10 +3,18 @@ const ordenarActividades = (actividades, orden) => {
   const actividadesOrdenadas = actividades.filter((actividad) => {
     const actividadDate = new Date(actividad.actividad.proximoContacto);
     if (orden === 1) {
-      return actividadDate <= today;
+      return (
+        actividadDate.getFullYear() <= today.getFullYear() &&
+        actividadDate.getMonth() <= today.getMonth() &&
+        actividadDate.getDate() <= today.getDate()
+      );
     }
     if (orden === 2) {
-      return actividadDate < today && !esMismoDia(actividadDate, today);
+      return (
+        actividadDate.getFullYear() < today.getFullYear() &&
+        actividadDate.getMonth() < today.getMonth() &&
+        actividadDate.getDate() < today.getDate()
+      );
     }
     if (orden === 3) {
       return (
@@ -16,20 +24,16 @@ const ordenarActividades = (actividades, orden) => {
       );
     }
     if (orden === 4) {
-      return actividadDate > today;
+      return (
+        actividadDate.getFullYear() >= today.getFullYear() &&
+        actividadDate.getMonth() >= today.getMonth() &&
+        actividadDate.getDate() >= today.getDate()
+      );
     }
     return true; // orden === 5: Todas las actividades
   });
 
   return actividadesOrdenadas;
-};
-
-const esMismoDia = (date1, date2) => {
-  return (
-    date1.getFullYear() === date2.getFullYear() &&
-    date1.getMonth() === date2.getMonth() &&
-    date1.getDate() === date2.getDate()
-  );
 };
 
 export default ordenarActividades;
